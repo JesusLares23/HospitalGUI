41533241c88664586d12726a50628e1246ef0c69
JesusLares23 <jesus.lares233383@potros.itson.edu.mx>
2025-02-26 19:03:09 -0700

    Copiado de base de proyecto

---
+++ src/main/java/com/mycompany/hospitalgui/HospitalGUI.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hospitalgui;
+
+import Menu.MenuPrincipal;
+import Persistencia.*;
+
+
+/**
+ *
+ * @author Jesús Pedro Lares Velncia (00000233383) y
+ */
+public class HospitalGUI {
+
+    public static void main(String[] args) {
+        IPersistenciaFachada persistencia = new PersistenciaFachada();
+        MenuPrincipal menu = new MenuPrincipal(persistencia);
+        menu.mostrar();
+    }
+}
